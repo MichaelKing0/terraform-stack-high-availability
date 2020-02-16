0351dfdd3b431e03d6774c867f083b7c574c2687
michaelking <michael@mkwd.net>
2020-02-16 12:41:59 +0000

    Create public route table and associations

--- main.tf
+++ main.tf
@@ -20,3 +20,26 @@ resource "aws_internet_gateway" "gw" {
     Name = "HA IGW"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "HA Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "eu-west-1a-public" {
+  subnet_id = "${module.subnet.subnet-eu-west-1a-public.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "eu-west-1b-public" {
+  subnet_id = "${module.subnet.subnet-eu-west-1b-public.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
